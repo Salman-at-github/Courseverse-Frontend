878a4fdf8f8245011b592afd794b92b448554178
Salman <salmankps2001@gmail.com>
2023-12-13 21:12:26 +0530

    Fixed layout issue on skeleton

--- src/components/skeletons/CoursesLoadingSkeleton.jsx
+++ src/components/skeletons/CoursesLoadingSkeleton.jsx
@@ -3,7 +3,7 @@ import SkeletonCourseCard from './SkeletonCourseCard';
 
 const Loading = () => {
   return (
-    <div className="className='min-h-screen bg-cyan-50 sm:px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-6 py-10 px-10'">
+    <div className="min-h-screen bg-cyan-50 sm:px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-6 py-10 px-10">
         {Array.from({length:8}).map((_, index)=>{
             return(
                 <SkeletonCourseCard key={index}/>
